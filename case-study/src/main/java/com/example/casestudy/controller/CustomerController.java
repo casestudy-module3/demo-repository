26920a317ba47b272a7aced6f905bf29e4ea42d9
KimHuong1901 <trankimhuong1901@gmail.com>
2024-12-10 08:00:25 +0700

    sessions

--- case-study/src/main/java/com/example/casestudy/controller/CustomerController.java
+++ case-study/src/main/java/com/example/casestudy/controller/CustomerController.java
@@ -9,29 +9,39 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.util.List;
 
 @WebServlet(name = "customerCotroller", value = "/customers")
 public class CustomerController extends HttpServlet {
     private static ICustomerService customerService = new CustomerService();
-
+    HttpSession session;
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
         String action = request.getParameter("action");
         if (action == null) action = "";
-
+        session = request.getSession();
+        boolean isInfor = (boolean) session.getAttribute("isInformation");
         switch (action) {
             case "search":
+                if(isInfor){
                 String name = request.getParameter("name");
                 List<Customer> customerByName = customerService.findByName(name);
                 request.setAttribute("customers", customerByName);
                 request.getRequestDispatcher("WEB-INF/view/customer.jsp").forward(request, response);
+                }else {
+                   request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+                }
                 break;
             default:
+                if(isInfor){
                 List<Customer> customerList = customerService.getAll();
                 request.setAttribute("customers", customerList);
                 request.getRequestDispatcher("WEB-INF/view/customer.jsp").forward(request, response);
+                }else {
+                    request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+                }
                 break;
 
         }
